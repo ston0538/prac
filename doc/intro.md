4ae0ed4dd237a1f49f4ba1302d7022e6020c539a
KHK <ston0538@gmail.com>
2020-08-26 02:24:50 +0900

    [UPDATE] Collection
    [notion](https://www.notion.so/khk0613/collection-d897b2272e93424f895709fae6fc88d4)

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to prac
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/prac/core.clj
@@ -0,0 +1,12 @@
+(ns prac.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+(comment
+  (+ 1 1)
+
+  ())
