7a0bd97b65427f77f1db8f409b46bfa28d0415da
KHK <ston0538@gmail.com>
2020-09-08 03:06:04 +0900

    [UPDATE] 2장 제어문과 함수형 반환 -> 논리 검사
    1. [논리에 따라 흐름 제어하기](https://www.notion.so/khk0613/63409b70d4a04311946571e652bf4202)
    2. [컬렉션에서 사용하는 논리 검사](https://www.notion.so/khk0613/2d36b3cb4a05419cbdbffb334dec6da5)

--- src/prac/prac.clj
+++ src/prac/prac.clj
@@ -1,10 +1,16 @@
 (ns prac.prac
   (:require [camel-snake-kebab.core :as csk]))
 
-
+(csk/->snake_case "hello world")
 (defn prac [input]
   (str "Serpent! You said: "
        (csk/->snake_case input)))
 
 (defn -main [& args]
   (println (prac (first args))))
+
+(defn drinkable? [x]
+  (= x :drinkme))
+(every? odd? [1 3 5])
+(every? drinkable? [:drinkme :drinkme])
+(every? drinkable? [:drinkme1 :drinkme])
