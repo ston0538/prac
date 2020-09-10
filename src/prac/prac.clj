9b19d615f9fca0f0c7d5d8d798f90e3cfb651f72
KHK <ston0538@gmail.com>
2020-09-11 02:58:09 +0900

    [UPDATE] 2장 제어문과 함수형 반환
    1. [흐름 제어 이용하기](https://www.notion.so/khk0613/f0256426db274dd59aca0b4d4c4a0089)

--- src/prac/prac.clj
+++ src/prac/prac.clj
@@ -14,3 +14,57 @@
 (every? odd? [1 3 5])
 (every? drinkable? [:drinkme :drinkme])
 (every? drinkable? [:drinkme1 :drinkme])
+
+(every? (fn [x] (= x :drinkme)) [:drinkme :drinkme])
+(every? #(= % :drinkme %) [:drinkme :drinkme])
+
+(not-any? odd? [1 3 5])
+(not-any? #(= % :drinkme %) [:drinkme1 :drinkme2])
+
+(some #(> % 3) [1 2 3 4 5])
+(some #(> % 3) [1 2 3])
+
+(#{1 2 3 4 5} 8)
+(contains? #{1 2 3 4 5} 5)
+
+(some #{3} [1 2 3 4 5])
+(some #{4 5} [1 2 3])
+(some #{nil} [nil nil])
+(some #{false} [false false false])
+
+(if true "It is true" "It tis false")
+(if (= :drinkme :KHK) "true", "false")
+
+(let [developer "Alice in Wonderland"])
+(if-let [need-to-grow-small (> 5 1)] "drink bottle" "drink coffee")
+
+(defn drink [need-to-grow-small] (when need-to-grow-small "drink bottle"))
+(drink true)
+(drink false)
+
+(when-let [need-to-grow-small true] "drink bottle")
+(when-let [need-to-grow-small false] "drink bottle")
+
+(let [bottle "drinkme"]
+  (cond (= bottle "poison") "don't touch"
+        (= bottle "drinkme") "grow smaller"
+        (= bottle "empty") "all gone"))
+
+(let [x 5]
+  (cond
+    (> x 10) "bigger then 10"
+    (> x 4) "bigger then 4"
+    (> x 3) "bigger then 3"))
+
+(let [x 5]
+  (cond
+    (> x 10) "bigger then 10"
+    (> x 11) "bigger then 4"
+    (> x 123) "bigger then 3") :else "unknown")
+
+(let [bottle "drinkmes"]
+  (case bottle
+    "poison" "don't touch"
+    "drinkme" "grow smaller"
+    "empty" "all gone"
+    "hi"))
