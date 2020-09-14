a23f8a41e841ff16f1ea9d2552ee239cd6b2fe5c
KHK <ston0538@gmail.com>
2020-09-15 04:04:50 +0900

    [UPDATE] 2장 제어문과 함수형 반환
    1. [함수를 만드는 함수](https://www.notion.so/khk0613/fc8074c15c2a40139a3daac92d9c838f)
    
    2. [구조분해](https://www.notion.so/khk0613/18611de493ab4ba1b4b11cf239eb0dd4)

--- src/prac/prac.clj
+++ src/prac/prac.clj
@@ -68,3 +68,90 @@
     "drinkme" "grow smaller"
     "empty" "all gone"
     "hi"))
+
+
+; currying
+; 1.partial
+(defn grow [name direction]
+  (if (= direction :small)
+    (str name " is growing smaller")
+    (str name " is growing bigger")))
+(grow "Alice" :small)
+(grow "Alice" :big)
+
+(partial grow "Alice")
+((partial grow "Alice") :small)
+
+; 2.comp
+(defn toggle-grow [direction] (if (= direction :small) :big :small))
+
+(toggle-grow :big)
+(toggle-grow :small)
+
+(defn oh-my [direction] (str "Oh My! You are growing " direction))
+
+(oh-my (toggle-grow :big))
+
+(defn surprise [direction] ((comp oh-my toggle-grow) direction))
+
+(surprise :small)
+
+(defn adder [x y] (+ x y))
+
+(adder 3 4)
+
+(def adder-5 (partial adder 5))
+
+((partial adder 5) 10)
+
+(adder-5 10)
+
+; destructure
+; let basic
+(let [developer "Alice" rabbit "Alick Rabbit"] [developer rabbit])
+
+
+; vector before
+(let [color "blue" size 12] (str "The " color " door is " size))
+
+(let [x ["blue", "small"]
+      color (first x)
+      size (last x)]
+  (str "The " color " door is " size))
+
+; vector after 
+(let [[color size] ["blue" 12]]
+  (str "The " color " door is " size))
+
+(let [[color [size] :as original] ["blue" [12]]]
+  {:color color :size size :original original})
+
+; map basic
+{:jam1 "apple" :jam2 "melon"}
+
+; map destructure
+(let [{flower1 :flower1 flower2 :flower2} {:flower1 "red" :flower2 "blue"}]
+  (str "The flowers are " flower1 " and " flower2))
+
+; map destructure "or"
+(let [{flower1 :flower1 flower2 :flower2 :or {flower2 "missing"}} {:flower1 "red"}]
+  (str "The flowers are " flower1 " and " flower2))
+
+; map destructure "as"
+(let [{flower1 :flower1 :as all-flowers} {:flower1 "red"}]
+  [flower1 all-flowers])
+
+; map destructure "keys"
+(let [{:keys [flower1 flower2]} {:flower1 "red" :flower2 "blue"}]
+  (str "The flowers are " flower1 " and " flower2))
+
+; function args destructure
+; before
+(defn flower-colors [colors]
+  (str "The flower are " (:flower1 colors) " and " (:flower2 colors)))
+
+(flower-colors {:flower1 "red" :flower2 "blue"})
+
+(defn flower-colors [{:keys [flower1 flower2]}]
+
+  (str "The flowers are " flower1 " and " flower2))
