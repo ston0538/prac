efba018d2656dac2e342089321617ed9623b324d
KHK <ston0538@gmail.com>
2020-09-15 04:07:50 +0900

    [UPDATE] 2장 제어문과 함수형 반환
    1. [함수를 만드는 함수](https://www.notion.so/khk0613/fc8074c15c2a40139a3daac92d9c838f)
    
    2. [구조분해](https://www.notion.so/khk0613/18611de493ab4ba1b4b11cf239eb0dd4)

--- src/prac/prac.clj
+++ src/prac/prac.clj
@@ -152,6 +152,8 @@
 
 (flower-colors {:flower1 "red" :flower2 "blue"})
 
+; after
 (defn flower-colors [{:keys [flower1 flower2]}]
-
   (str "The flowers are " flower1 " and " flower2))
+
+(flower-colors {:flower1 "red" :flower2 "blue"})
